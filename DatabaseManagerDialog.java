a257aec3bb71beb1743c9555521077ed5b06173a
cvosteen <cvosteen@78eabd0e-0761-11df-8636-19de83dc8dca>
2010-01-22 15:37:41 +0000

    Initial commit... hopefully correct this time :)

---
+++ DatabaseManagerDialog.java
@@ -0,0 +1,193 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+
+public class DatabaseManagerDialog extends JDialog {
+
+	private final DatabaseManager databaseManager;
+	private JList list;
+	private JPopupMenu popup;
+	private Database response = null;
+
+	public DatabaseManagerDialog(Frame owner, DatabaseManager theDatabaseManager) {
+		super(owner, "Databases", true);
+
+		if(theDatabaseManager == null)
+			throw new NullPointerException("I need a DatabaseManager, duh!");
+		this.databaseManager = theDatabaseManager;
+		
+		// We want to hide this object when we click on the 'X'
+		// After this dialog has been closed, we will need to retreive the chosen database
+		setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
+
+		// Set the layout (GridBag)
+		JPanel panel = new JPanel();
+		GridBagLayout gridbag = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		panel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		panel.setLayout(gridbag);
+		c.fill = GridBagConstraints.BOTH;
+		c.insets = new Insets(5,5,5,5);
+	
+		// Add the list to take up the whole left side of the dialog
+		c.weightx = 1.0;
+		c.weighty = 1.0;
+		c.gridwidth = 1;
+		c.gridheight = 3;
+		list = new JList();
+		list.setListData(databaseManager.getAllDatabases().toArray());
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		list.setSelectedIndex(0);
+
+		// Add a popup menu to the list
+		popup = new JPopupMenu();
+		JMenuItem newMenuItem = new JMenuItem("New...");
+		newMenuItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					newDatabase();
+				}
+			});
+		JMenuItem editMenuItem = new JMenuItem("Edit...");
+		editMenuItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					editDatabase();
+				}
+			});
+		JMenuItem delMenuItem = new JMenuItem("Delete...");
+		delMenuItem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					deleteDatabase();
+				}
+			});
+		popup.add(newMenuItem);
+		popup.add(editMenuItem);
+		popup.add(delMenuItem);
+		list.addMouseListener(new MouseAdapter() {
+				// A right click will bring up the context menu
+				public void mousePressed(MouseEvent e) {
+					if(e.isPopupTrigger())
+						popup.show(e.getComponent(), e.getX(), e.getY());
+				}
+
+				public void mouseReleased(MouseEvent e) {
+					if(e.isPopupTrigger())
+						popup.show(e.getComponent(), e.getX(), e.getY());
+				}
+				// A double click will connect
+				public void mouseClicked(MouseEvent e) {
+					if(e.getClickCount() == 2) {
+						response = (Database) list.getSelectedValue();					
+						dispose();
+					}	
+				}
+			});
+
+		JScrollPane scrollPane = new JScrollPane(list);
+		gridbag.setConstraints(scrollPane, c);
+		panel.add(scrollPane);
+
+		// Two buttons on the upper right side
+		c.weightx = 0.0;
+		c.weighty = 0.0;
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		c.gridheight = 1;
+		JButton connectButton = new JButton("Connect");
+		connectButton.setDefaultCapable(true);
+		connectButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					response = (Database) list.getSelectedValue();					
+					dispose();
+				}
+			});
+		gridbag.setConstraints(connectButton, c);
+		panel.add(connectButton);
+		JButton cancelButton = new JButton("Cancel");
+		// When escape is pressed, activate the Cancel button
+		cancelButton.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "doCancel");
+		cancelButton.getActionMap().put("doCancel", new AbstractAction() {
+				public void actionPerformed(ActionEvent e) {
+					dispose();
+				}
+			});
+		cancelButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					dispose();
+				}
+			});
+		gridbag.setConstraints(cancelButton, c);
+		panel.add(cancelButton);
+
+		getContentPane().add(panel);
+		setMinimumSize(new Dimension(300,200));
+		pack();
+		list.requestFocusInWindow();
+		getRootPane().setDefaultButton(connectButton);
+
+		// center the window on the screen
+		Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation((screenDim.width-getWidth())/2,
+			(screenDim.height-getHeight())/2);
+
+		setVisible(true);
+	}
+	
+	private void newDatabase() {
+		DatabaseDialog dbDialog = new DatabaseDialog(this);
+		Database newDatabase = dbDialog.getResponse();
+		if(newDatabase != null) {
+			databaseManager.saveDatabase(newDatabase);
+			try {
+				databaseManager.save();
+			} catch(IOException e) {
+				JOptionPane.showMessageDialog(this,
+					e.getMessage(), "Error",
+					JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		list.setListData(databaseManager.getAllDatabases().toArray());
+	}
+
+	private void editDatabase() {
+		Database selection = (Database) list.getSelectedValue();
+		if(selection != null) {
+			DatabaseDialog dbDialog = new DatabaseDialog(this, selection);
+			Database newDatabase = dbDialog.getResponse();
+			if(newDatabase != null) {
+				selection.setName(newDatabase.getName());
+				selection.setDriver(newDatabase.getDriver());
+				selection.setConnectionUrl(newDatabase.getConnectionUrl());
+				try {
+					databaseManager.save();
+				} catch(IOException e) {
+					JOptionPane.showMessageDialog(this,
+						e.getMessage(), "Error",
+						JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		}
+		list.setListData(databaseManager.getAllDatabases().toArray());
+	}
+
+	private void deleteDatabase() {
+		Database selection = (Database) list.getSelectedValue();
+		if(selection != null && JOptionPane.showConfirmDialog(
+				list, "Are you sure you want to delete " + selection.getName() + "?",
+				"Delete Database", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE)
+				== JOptionPane.YES_OPTION) {
+			databaseManager.deleteDatabase((Database) list.getSelectedValue());
+			try {
+				databaseManager.save();
+			} catch(IOException e) {
+				JOptionPane.showMessageDialog(this,
+					e.getMessage(), "Error",
+					JOptionPane.ERROR_MESSAGE);
+			}
+			list.setListData(databaseManager.getAllDatabases().toArray());
+		}
+	}
+
+	public Database getResponse() {
+		return response;
+	}
+}
---
+++ JTablePrintable.java
@@ -0,0 +1,180 @@
+import java.awt.*;
+import java.awt.print.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.table.*;
+
+public class JTablePrintable implements Printable {
+
+	JTable table;
+	ArrayList<TableEntity> entities = null;
+	int maxPageX = 0;
+	int maxPageY = 0;
+	final Font plainFont = new Font("Arial", Font.PLAIN, 8);
+	final Font boldFont = new Font("Arial", Font.BOLD, 8);
+	final int yPadding = 0;
+	final int xPadding = 3;
+
+	
+	public JTablePrintable(JTable table) {
+		this.table = table;
+	}
+
+	public int print(Graphics g, PageFormat pf, int page) throws PrinterException {
+		// Translate so it fits within the imageable area
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.translate(pf.getImageableX(), pf.getImageableY());
+		
+		// Set the font, default it too big
+		g.setFont(plainFont);
+
+		// All work should be done here to list all entities and their coordinates
+		// and page numbers
+		if(entities == null) {
+			entities = new ArrayList<TableEntity>();
+			TableModel model = table.getModel();
+			
+			// Collect table entities one column at a time
+			int lineheight = g.getFontMetrics().getHeight() + (2 * yPadding);
+			int currentX = 0;
+			for(int col = 0; col < model.getColumnCount(); col++) {
+				// First calc the column's width
+				int maxWidth = 0;
+				String thisString = model.getColumnName(col);
+				int thisWidth = g.getFontMetrics(boldFont).stringWidth(thisString);
+				if(thisWidth > maxWidth)
+					maxWidth = thisWidth;
+				for(int row = 0; row < model.getRowCount(); row++) {
+					Object value = model.getValueAt(row, col);
+					if(value == null)
+						value = "";
+					thisString = value.toString();
+					thisWidth = g.getFontMetrics().stringWidth(thisString);
+					if(thisWidth > maxWidth)
+						maxWidth = thisWidth;
+				}
+				maxWidth += (2 * xPadding);
+
+				// Now create the column's entities
+				thisString = model.getColumnName(col);
+				entities.add(new TableEntity(thisString, true, true, currentX, 0, maxWidth, lineheight, -1, -1));
+				for(int row = 0; row < model.getRowCount(); row++) {
+					Object value = model.getValueAt(row, col);
+					if(value == null)
+						value = "";
+					thisString = value.toString();
+					entities.add(new TableEntity(thisString, false, true, currentX, (row + 1) * lineheight, maxWidth, lineheight, -1, -1));
+				}
+				currentX += maxWidth;
+			}
+
+			// Paginate the entities vertically
+			int pageHeight = (int) pf.getImageableHeight();
+			int pageY = 0;
+			int minY = 0;
+			boolean moreEntities = true;
+			while(moreEntities) {
+				int newMinY = 2000000000;
+				moreEntities = false;
+				for(TableEntity e : entities) {
+					if(e.pageY == -1) {
+						// Adjust height by previous page's content's height
+						e.y -= minY;
+						// Find all items on current page
+						if(e.y + e.height < pageHeight || e.y == 0) {
+							e.pageY = pageY;
+						} else {
+							moreEntities = true;
+							if(e.y < newMinY)
+								newMinY = e.y;
+						}
+					}
+				}
+				minY = newMinY;
+				pageY++;
+			}
+			maxPageY = pageY - 1;
+
+			// Paginate the entities horizontally
+			int pageWidth = (int) pf.getImageableWidth();
+			int pageX = 0;
+			int minX = 0;
+			moreEntities = true;
+			while(moreEntities) {
+				int newMinX = 2000000000;
+				moreEntities = false;
+				for(TableEntity e : entities) {
+					if(e.pageX == -1) {
+						// Adjust height by previous page's content's height
+						e.x -= minX;
+						// Find all items on current page
+						if(e.x + e.width < pageWidth || e.x == 0) {
+							e.pageX = pageX;
+						} else {
+							moreEntities = true;
+							if(e.x < newMinX)
+								newMinX = e.x;
+						}
+					}
+				}
+				minX = newMinX;
+				pageX++;
+			}
+			maxPageX = pageX - 1;
+			// Calc total height in pages (easy)
+			// maxPageX = (int) (pf.getImageableHeight() / (lineheight * model.getRowCount())) + 1;
+
+		}
+
+		// For now let's only print one page
+		if(page > ((maxPageY + 1) * (maxPageX + 1)) - 1)
+			return NO_SUCH_PAGE;
+
+		// Now print the table entities
+		for(TableEntity e : entities) {
+			if(page == (e.pageX * (maxPageY + 1)) + e.pageY) {
+				if(e.bold)
+					g.setFont(boldFont);
+
+				FontMetrics fm = g.getFontMetrics();
+				int ypad = yPadding + fm.getLeading() + fm.getAscent();
+				g.drawString(e.text, e.x + xPadding, e.y + ypad);
+
+				if(e.boxed)
+					g.drawRect(e.x, e.y, e.width, e.height);
+
+				if(e.bold)
+					g.setFont(plainFont);
+			}
+		}
+		
+		return PAGE_EXISTS;
+	}
+
+	public class TableEntity {
+		public String text;
+		public boolean bold;
+		public boolean boxed;
+		public int x;
+		public int y;
+		public int width;
+		public int height;
+		public int pageX;
+		public int pageY;
+
+		public TableEntity(String text, boolean bold, boolean boxed, int x, int y, int width, int height,
+				int pageX, int pageY) {
+			this.text = text;
+			this.bold = bold;
+			this.boxed = boxed;
+			this.x = x;
+			this.y = y;
+			this.width = width;
+			this.height = height;
+			this.pageX = pageX;
+			this.pageY = pageY;
+		}
+	}
+
+}
+	
