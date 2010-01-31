6ad0ef7c69a3028d08435ea5b94f393b8c1a5d67
cvosteen <cvosteen@78eabd0e-0761-11df-8636-19de83dc8dca>
2010-01-31 18:57:25 +0000

    The licence is now a resource and not hard-coded in AboutDialog.java.

--- gui/AboutDialog.java
+++ gui/AboutDialog.java
@@ -12,37 +12,6 @@ import javax.swing.text.*;
 
 public class AboutDialog extends JDialog {
 
-	private static final String license =
-		"<font face=\"Arial\" size=\"2\">" +
-		"Copyright (c) 2009, Christian Vosteen<br />" +
-		"All rights reserved.<br /><br />" +
-		"<p>Redistribution and use in source and binary forms, with " +
-		"or without modification, are permitted provided that the " +
-		"following conditions are met:</p>" +
-		"<ul><li>Redistributions of source code must retain the above " +
-		"copyright notice, this list of conditions and the following " +
-		"disclaimer.</li>" +
-		"<li>Redistributions in binary form must reproduce the above " +
-		"copyright notice, this list of conditions and the following " +
-		"disclaimer in the documentation and/or other materials provided " +
-		"with the distribution.</li>" +
-		"<li>The names of its contributors may not be used to endorse or " +
-		"promote products derived from this software without specific " +
-		"prior written permission.</li></ul>" +
-		"<p>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS " +
-		"\"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT " +
-		"NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND " +
-		"FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT " +
-		"SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY " +
-		"DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL " +
-		"DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE " +
-		"GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS " +
-		"INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, " +
-		"WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING " +
-		"NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF " +
-		"THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH " +
-		"DAMAGE.</p></font>";
-
 	public AboutDialog(Frame owner, String name, String version) {
 		super(owner, "About " + name, true);
 
@@ -87,7 +56,15 @@ public class AboutDialog extends JDialog {
 		c.weightx = 1.0;
 		c.weighty = 1.0;
 		c.gridwidth = GridBagConstraints.REMAINDER;
-		JEditorPane text = new JEditorPane("text/html", license);
+		JEditorPane text = null;
+		try {
+			text = new JEditorPane(this.getClass().getResource("/license.html"));
+		} catch(IOException e) {
+			JOptionPane.showMessageDialog(this,
+				"Unable to read licence.html", "Error",
+				JOptionPane.ERROR_MESSAGE);
+			text = new JEditorPane();
+		}
 		text.setCaretPosition(0);
 		text.setEditable(false);
 		Dimension size = text.getPreferredSize();
@@ -101,7 +78,7 @@ public class AboutDialog extends JDialog {
 		getRootPane().setDefaultButton(button);
 		button.requestFocusInWindow();
 		getContentPane().add(panel);
-		setMinimumSize(new Dimension(300,200));
+		setMinimumSize(new Dimension(300,250));
 		pack();
 
 		// center the window on the screen
@@ -110,7 +87,6 @@ public class AboutDialog extends JDialog {
 			(screenDim.height-getHeight())/2);
 
 		setVisible(true);
-
 	}
 	
 }
