44ccfbc391305fbdc972a15bbc96d69be567317f
cvosteen <cvosteen@78eabd0e-0761-11df-8636-19de83dc8dca>
2011-04-28 17:55:00 +0000

    Added capability of JTablePrintable to scale the table size and to fit to a specified number of pages wide or high.  A GUI for this still needs to be created.

--- cvosteen/sqltool/gui/components/JTablePrintable.java
+++ cvosteen/sqltool/gui/components/JTablePrintable.java
@@ -17,22 +17,25 @@ import javax.swing.table.*;
 public class JTablePrintable implements Printable {
 
 	ArrayList<TextEntity> entities = null;
-	String title = "";
 	TextEntity titleEntity = null;
 	TextEntity timestampEntity = null;
+	boolean initialized = false;
 	
-	int maxPageX = 0;
-	int maxPageY = 0;
-	final Font plainFont = new Font("Arial", Font.PLAIN, 8);
-	final Font boldFont = new Font("Arial", Font.BOLD, 8);
-	final int yPadding = 0;
-	final int xPadding = 3;
+	// Table data
+	private String title = "";
+	private GraphicalTable table;
 
-	// ****** NEW CODE HERE?!?!?
-	private int numColumns;
-	private int numRows;
-	private java.util.List<String> columns;
-	private java.util.List<java.util.List<String>> data;
+	// Fonts
+	private Font plainFont = new Font("Arial", Font.PLAIN, 10);
+	private Font boldFont = new Font("Arial", Font.BOLD, 10);
+
+	// Zooming and Fitting options
+	private double zoom = 1.0;
+	private boolean fitWidth = false;
+	private int fitWidthPages = 0;
+	private boolean fitHeight = false;
+	private int fitHeightPages = 0;
+	private final double zoomReadjust = 0.95;
 
 	
 	public JTablePrintable(JTable table) {
@@ -52,97 +55,119 @@ public class JTablePrintable implements Printable {
 	}
 
 	/**
+	 * Explicitly set the zoom for printing this table
+	 * The default is 100%
+	 */
+	public void setZoom(double zoom) {
+		this.zoom = zoom;
+		fitWidth = false;
+		fitHeight = false;
+	}
+
+	/**
+	 * Sets the number of pages that the table must
+	 * fit into width-wise
+	 */
+	public void setFitWidth(int pages) {
+		this.zoom = 1.0;
+		fitWidth = true;
+		fitWidthPages = pages;
+	}
+
+	/**
+	 * Sets the number of pages that the table must
+	 * fit into height-wise
+	 */
+	public void setFitHeight(int pages) {
+		this.zoom = 1.0;
+		fitHeight = true;
+		fitHeightPages = pages;
+	}
+
+	/**
 	 * Implement the printable interface.
 	 * Most of the calculations must take place here because we won't know
 	 * the size of the page etc. until print time.
 	 */
 	public int print(Graphics g, PageFormat pf, int page) throws PrinterException {
-		// TODO: Implement some sort of page fitting/zooming
-
 		// Translate so it fits within the imageable area
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.translate(pf.getImageableX(), pf.getImageableY());
 		
-		// Set the font, default is too big
+		// Set the font
 		g.setFont(plainFont);
-		
-		// Standard height of a line of text
-		int lineheight = g.getFontMetrics().getHeight() + (2 * yPadding);
-		
-		// The area within the page that the table will take up
-		// leaving room for headers and footers
-		Rectangle tableRect = new Rectangle(0, 2 * lineheight, (int) pf.getImageableWidth(), (int) pf.getImageableHeight() - (4 * lineheight));
 
-		// Number of table rows that will fit in a page
-		int rowsPerPage = tableRect.height / lineheight;
-
+		// Get the standard line height
+		int lineHeight = g.getFontMetrics().getHeight();
+		
 		// All work should be done here to list all entities and their coordinates
 		// and page numbers.  Ensure that these calculations are done only for the first
 		// page request.  After that use the already calculated data.
-		if(entities == null) {
-			entities = new ArrayList<TextEntity>();
-		
+		if(!initialized) {
+			initialized = true;
+
+			// The area within the page that the table will take up
+			// leaving room for headers and footers
+			Rectangle tableRect = new Rectangle(0, 2 * lineHeight, (int) pf.getImageableWidth(), (int) pf.getImageableHeight() - (4 * lineHeight));
+			System.out.println("Print Area: " + tableRect.x + ", " + tableRect.y + ", " + tableRect.width + ", " + tableRect.height);
+
+			// Set these parameters so the table can make needed calculations
+			table.setPageArea(tableRect);
+			table.setGraphics(g);
+
+			// Set the zoom of the table
+			table.setZoom(zoom);
+
+			// Do we have page restrictions?
+			if(fitWidth)
+			{
+				boolean fits = table.getPagesWide() <= fitWidthPages;
+				while(!fits)
+				{
+					zoom *= zoomReadjust;
+					table.setZoom(zoom);
+					fits = table.getPagesWide() <= fitWidthPages;
+				}
+			}
+			if(fitHeight)
+			{
+				boolean fits = table.getPagesHigh() <= fitHeightPages;
+				while(!fits)
+				{
+					zoom *= zoomReadjust;
+					table.setZoom(zoom);
+					fits = table.getPagesHigh() <= fitHeightPages;
+				}
+			}
+
 			// Generate the title entity
 			g.setFont(boldFont);
 			int titleWidth = g.getFontMetrics().stringWidth(title);
 			int titleX = (int)(pf.getImageableWidth() / 2) - (titleWidth / 2);
-			titleEntity = new TextEntity(title, true, false, titleX, 0, titleWidth, lineheight, -1, -1);
+			titleEntity = new TextEntity(title, boldFont, null, titleX, 0, titleWidth, lineHeight, 0, 0);
 			
 			// Generate the timestamp entity
 			g.setFont(plainFont);
 			String timestamp = new SimpleDateFormat("EEE, MMM d, yyyy   h:mm a").format(new Date());
 			int timestampWidth = g.getFontMetrics().stringWidth(timestamp);
-			int timestampY = (int) pf.getImageableHeight() - lineheight;
-			timestampEntity = new TextEntity(timestamp, false, false, 0, timestampY, timestampWidth, lineheight, -1, -1);
-
-			// Collect table entities one column at a time
-			// Keep track of the x coordinate as we traverse each column
-			// The y coordinate is just a multiple of lineheight
-			// The vertical page number is also easly calculated
-			int currentX = tableRect.x;
-			int pageX = 0;
-			for(int col = 0; col < numColumns; col++) {
-				int colWidth = getColumnWidth(g, col);
-				colWidth += (2 * xPadding);
-
-				// Advance pageX if needed
-				if(currentX + colWidth > tableRect.width && currentX > 0) {
-					currentX = 0;
-					pageX++;
-				}
-
-				// Now create the column's entities
-				String thisString = columns.get(col);
-				entities.add(new TextEntity(thisString, true, true, currentX, tableRect.y, colWidth, lineheight, pageX, 0));
-				for(int row = 0; row < numRows; row++) {
-					thisString = data.get(row).get(col);
-					int pageY = (row + 1) / rowsPerPage; // Add 1 for the headers
-					int y = ((row + 1) - (pageY * rowsPerPage)) * lineheight + tableRect.y;
-					entities.add(new TextEntity(thisString, false, true, currentX, y, colWidth, lineheight, pageX, pageY));
-				}
-				currentX += colWidth;
-			}
-			maxPageX = pageX;
-			maxPageY = (numRows + 1) / rowsPerPage;
-
-			// Generate the Page x of y footers
-			g.setFont(plainFont);
-			int totalPages = ((maxPageY + 1) * (maxPageX + 1));
-			for(int x = 0; x <= maxPageX; x++) {
-				for(int y = 0; y <= maxPageX; y++) {
-					int pageNum = (x * (maxPageY + 1)) + y + 1;
-					String pageFooter = "Page " + pageNum + " of " + totalPages;
-					int pageFooterY = (int) pf.getImageableHeight() - lineheight;
-					int pageFooterWidth = g.getFontMetrics().stringWidth(pageFooter);
-					int pageFooterX = (int) pf.getImageableWidth() - pageFooterWidth - xPadding;
-					entities.add(new TextEntity(pageFooter, false, false, pageFooterX, pageFooterY, pageFooterWidth, lineheight, x, y));
-				}
-			}
+			int timestampY = (int) pf.getImageableHeight() - lineHeight;
+			timestampEntity = new TextEntity(timestamp, plainFont, null, 0, timestampY, timestampWidth, lineHeight, 0, 0);
 
 		}
+		
+		
+		java.util.List<TextEntity> entities = table.getEntities(page);
+
+		// Generate the Page x of y footer
+		g.setFont(plainFont);
+		String pageFooter = "Page " + (page+1) + " of " + table.getTotalPages();
+		int pageFooterWidth = g.getFontMetrics().stringWidth(pageFooter);
+		int pageFooterX = (int) pf.getImageableWidth() - pageFooterWidth;
+		int pageFooterY = (int) pf.getImageableHeight() - lineHeight;
+		entities.add(new TextEntity(pageFooter, plainFont, null, pageFooterX, pageFooterY, pageFooterWidth, lineHeight, 0, 0));
 
 		// For now let's only print one page
-		if(page > ((maxPageY + 1) * (maxPageX + 1)) - 1)
+		if(page >= table.getTotalPages())
 			return NO_SUCH_PAGE;
 
 		// This shouldn't happen, but better safe than sorry :P
@@ -153,9 +178,7 @@ public class JTablePrintable implements Printable {
 		titleEntity.draw(g);
 		timestampEntity.draw(g);
 		for(TextEntity e : entities) {
-			if(page == (e.pageX * (maxPageY + 1)) + e.pageY) {
-				e.draw(g);
-			}
+			e.draw(g);
 		}
 		
 		return PAGE_EXISTS;
@@ -163,17 +186,17 @@ public class JTablePrintable implements Printable {
 
 
 	/**
-	 * Extracts data from a JTable as a 2D String Array
+	 * Extracts data from a JTable as a list of lists
 	 * and saves the data as well as the dimensions in this
 	 * instance's private variables.
 	 */
-	private void extractFromJTable(JTable table) {
-		TableModel model = table.getModel();
+	private void extractFromJTable(JTable jtable) {
+		TableModel model = jtable.getModel();
 
-		numColumns = model.getColumnCount();
-		numRows = model.getRowCount();
-		columns = new ArrayList<String>();
-		data = new ArrayList<java.util.List<String>>();
+		int numColumns = model.getColumnCount();
+		int numRows = model.getRowCount();
+		java.util.List<String> columns = new ArrayList<String>();
+		java.util.List<java.util.List<String>> data = new ArrayList<java.util.List<String>>();
 
 		for(int col = 0; col < numColumns; col++) {
 			columns.add(model.getColumnName(col));
@@ -189,68 +212,306 @@ public class JTablePrintable implements Printable {
 			}
 			data.add(dataRow);
 		}
+
+		table = new GraphicalTable(columns, data);
 	}
 	
-	
+
 	/**
-	 * Calculates how wide a column needs to be to fit all of the
-	 * data for that column.
+	 * A text object that knows how to draw itself.
 	 */
-	private int getColumnWidth(Graphics g, int column) {
-		int maxWidth = 0;
-		// First get the column heading width
-		String thisString = columns.get(column);
-		int thisWidth = g.getFontMetrics(boldFont).stringWidth(thisString);
-		maxWidth = thisWidth;
-
-		// Go row by row measuring each item's width
-		for(int row = 0; row < numRows; row++) {
-			thisString = data.get(row).get(column);
-			thisWidth = g.getFontMetrics(plainFont).stringWidth(thisString);
-			if(thisWidth > maxWidth)
-				maxWidth = thisWidth;
-		}
-		return maxWidth;
-	}
-
 	public class TextEntity {
 		public String text;
-		public boolean bold;
-		public boolean boxed;
+		public Font font;
+		public Stroke boxStroke;
 		public int x;
 		public int y;
 		public int width;
 		public int height;
-		public int pageX;
-		public int pageY;
+		public int xPad;
+		public int yPad;
 
-		public TextEntity(String text, boolean bold, boolean boxed, int x, int y, int width, int height,
-				int pageX, int pageY) {
+		public TextEntity(String text, Font font, Stroke boxStroke, int x, int y, int width, int height,
+				int xPad, int yPad) {
 			this.text = text;
-			this.bold = bold;
-			this.boxed = boxed;
+			this.font = font;
+			this.boxStroke = boxStroke;
 			this.x = x;
 			this.y = y;
 			this.width = width;
 			this.height = height;
-			this.pageX = pageX;
-			this.pageY = pageY;
+			this.xPad = xPad;
+			this.yPad = yPad;
 		}
 
 		public void draw(Graphics g) {
-			if(bold)
-				g.setFont(boldFont);
-			else
-				g.setFont(plainFont);
-
+			g.setFont(font);
 			FontMetrics fm = g.getFontMetrics();
-			int ypad = yPadding + fm.getLeading() + fm.getAscent();
-			g.drawString(text, x + xPadding, y + ypad);
+			int yadj = yPad + fm.getLeading() + fm.getAscent();
+			g.drawString(text, x + xPad, y + yadj);
 
-			if(boxed)
+			if(boxStroke != null) {
+				Graphics2D g2d = (Graphics2D) g;
+				g2d.setStroke(boxStroke);
 				g.drawRect(x, y, width, height);
+			}
 		}
 	}
 
+
+	/**
+	 * Object which knows how to graphically split a table across multiple pages.
+	 */
+	public class GraphicalTable {
+		// The data in this table
+		private java.util.List<String> columns;
+		private java.util.List<java.util.List<String>> data;
+
+		// Extra parameters needed to calculate size and pages
+		private double zoom = 1.0; // Default zoom of 100%
+		private Rectangle pageArea = null;
+		private Graphics graphics = null;
+
+		// Padding within cells
+		final int yPadding = 1;
+		final int xPadding = 3;
+
+		// Fonts
+		private final int defaultFontSize = 10;
+		private final String defaultFontFamily = "Arial";
+		private Font plainFont = new Font("Arial", Font.PLAIN, defaultFontSize);
+		private Font boldFont = new Font("Arial", Font.BOLD, defaultFontSize);
+
+		// Column widths and row height at 100% zoom
+		private int[] columnWidths;
+		private int rowHeight;
+
+		/**
+		 * Creates a GraphicalTable instance with the specified column headings
+		 * and data.
+		 */		
+		public GraphicalTable(java.util.List<String> columns, java.util.List<java.util.List<String>> data) {
+			this.columns = columns;
+			this.data = data;
+		}
+
+		/**
+		 * Set the zoom factor. 1.0 is 100%
+		 */
+		public void setZoom(double zoom) {
+			this.zoom = zoom;
+		}
+
+		/**
+		 * Gets the zoom factor for this table.
+		 */
+		public double getZoom() {
+			return zoom;
+		}
+
+		/**
+		 * Sets the area this table will use to
+		 * calculate page boundaries
+		 */
+		public void setPageArea(Rectangle pageArea) {
+			this.pageArea = pageArea;
+		}
+
+		/**
+		 * Sets the graphics object.  Calculates and caches the
+		 * size of the rows and columns in the graphics context.
+		 */
+		public void setGraphics(Graphics graphics) {
+			this.graphics = graphics;
+			// Calculate column widths and row height at 100% zoom
+			int numColumns = getColumnCount();
+			columnWidths = new int[columns.size()];
+			for(int c = 0; c < numColumns; c++) {
+				columnWidths[c] = getColumnWidth(graphics, c) + (2 * xPadding);
+				System.out.println("Column width " + c + ": " + columnWidths[c]);
+			}
+			rowHeight = graphics.getFontMetrics().getHeight() + (2 * yPadding);
+		}
+
+		/**
+		 * For a specified graphics object, calculates the width of a column
+		 * by using the width of the widest cell in the column
+		 */
+		private int getColumnWidth(Graphics g, int column) {
+			int maxWidth = 0;
+			// First get the column heading width
+			String thisString = columns.get(column);
+			int thisWidth = g.getFontMetrics(boldFont).stringWidth(thisString);
+			maxWidth = thisWidth;
+
+			// Go row by row measuring each item's width
+			for(int row = 0; row < data.size(); row++) {
+				thisString = data.get(row).get(column);
+				thisWidth = g.getFontMetrics(plainFont).stringWidth(thisString);
+				if(thisWidth > maxWidth)
+					maxWidth = thisWidth;
+			}
+			return maxWidth;
+		}
+
+		/**
+		 * Returns the number of columns in this table.
+		 */
+		public int getColumnCount() {
+			return columns.size();
+		}
+
+		/**
+		 * Returns the number of rows in this table.
+		 */
+		public int getRowCount() {
+			return data.size();
+		}
+
+		/**
+		 * Returns the name of the specified column.
+		 */
+		public String getColumnName(int column) {
+			return columns.get(column);
+		}
+
+		/**
+		 * Returns the contents of the cell at the specified row and column.
+		 */
+		public String get(int row, int column) {
+			return data.get(row).get(column);
+		}
+
+		/**
+		 * Calculates the first column number to appear on
+		 * the specified x-axis page number.
+		 */
+		private int getFirstColumnOnPageX(int pageX) {
+			int currentPageX = 0;
+			int col = 0;
+			int pos = 0;
+			while(currentPageX < pageX && col < getColumnCount()) {
+				int width = (int)(columnWidths[col] * zoom);
+				if(width + pos > pageArea.width && pos != 0) {
+					// i.e. this column belongs in the next page
+					// unless the column already takes up the entire page!
+					currentPageX++;
+					pos = 0;
+				} else {
+					pos += width;
+					col++;
+				}
+			}
+			return col;
+		}
+
+		/**
+		 * Calculates the first row number to appear on
+		 * the specified y-axis page number.
+		 */
+		private int getFirstRowOnPageY(int pageY) {
+			int rowsPerPage = pageArea.height / (int)(rowHeight * zoom);
+			int firstRow = rowsPerPage * pageY - 1; // Subract 1 for header row
+			firstRow = firstRow < 0 ? 0 : firstRow;
+			firstRow = firstRow > getRowCount() ? getRowCount() : firstRow;
+			return firstRow;
+		}
+
+		/**
+		 * Calculates how many pages wide this table will be
+		 */
+		public int getPagesWide() {
+			int page = 0;
+			int pos = 0;
+			for(int c = 0; c < getColumnCount(); c++) {
+				int width = (int)(columnWidths[c] * zoom);
+				if(width + pos > pageArea.width && pos != 0) {
+					// i.e. this column belongs in the next page
+					// unless the column already takes up the entire page!
+					page++;
+					pos = width;
+				} else {
+					pos += width;
+				}
+			}
+			return page + 1;
+		}
+
+		/**
+		 * Calculates how many pages high this table will be
+		 */
+		public int getPagesHigh() {
+			int rowsPerPage = pageArea.height / (int)(rowHeight * zoom);
+			return ((data.size() + 1) / rowsPerPage) + 1;
+		}
+
+		/**
+		 * Returns the total pages that the table takes up
+		 */
+		public int getTotalPages() {
+			return getPagesWide() * getPagesHigh();
+		}
+
+		/**
+		 * Return a list of TextEntity objects that belon on the specified page number
+		 */
+		public java.util.List<TextEntity> getEntities(int page) {
+			if(pageArea == null) {
+				throw new MissingResourceException(
+					"This object needs the page area in order to make the needed calculations",
+					"Rectangle", "pageArea");
+			}
+			if(graphics == null) {
+				throw new MissingResourceException(
+					"This object needs a graphics object in order to make the needed calculations",
+					"Graphics", "graphics");
+			}
+			int pagesHigh = getPagesHigh();
+			int pageX = page / pagesHigh;
+			int pageY = page % pagesHigh;
+			
+			// Which columns and rows fall on this page
+			int colStart = getFirstColumnOnPageX(pageX);
+			int colEnd = getFirstColumnOnPageX(pageX + 1);
+			int rowStart = getFirstRowOnPageY(pageY);
+			int rowEnd = getFirstRowOnPageY(pageY + 1);
+			System.out.println("page " + page + ": row " + rowStart + " to " + rowEnd + ", col " + colStart + " to " + colEnd);
+
+			// Scale fonts down (or up) to zoom
+			Font scaledBold = new Font(defaultFontFamily, Font.BOLD, (int)(defaultFontSize * zoom));
+			Font scaledPlain = new Font(defaultFontFamily, Font.PLAIN, (int)(defaultFontSize * zoom));
+			
+			// If this is a "top" page, include the column headers
+			java.util.List<TextEntity> entities = new ArrayList<TextEntity>();
+			Stroke stroke = new BasicStroke((float)zoom);
+			int y = pageArea.y;
+			int height = (int)(rowHeight * zoom);
+			if(pageY == 0) {
+				int x = pageArea.x;
+				for(int col = colStart; col < colEnd; col++) {
+					int width = (int)(columnWidths[col] * zoom);
+					entities.add(new TextEntity(getColumnName(col), scaledBold, stroke, x, y, width, height,
+						(int)(xPadding * zoom), (int)(yPadding * zoom)));
+					x += width;
+				}
+				y += height;
+			}
+
+			// include table data
+			for(int row = rowStart; row < rowEnd; row++) {
+				int x = 0;
+				for(int col = colStart; col < colEnd; col++) {
+					int width = (int)(columnWidths[col] * zoom);
+					entities.add(new TextEntity(get(row, col), scaledPlain, stroke, x, y, width, height,
+						(int)(xPadding * zoom), (int)(yPadding * zoom)));
+					x += width;
+				}
+				y += height;
+			}
+
+			return entities;
+		}
+
+	}
 }
 	
