4d9037fa7b97f4f630f24ed7fd7d395149d7c5fe
cvosteen <cvosteen@78eabd0e-0761-11df-8636-19de83dc8dca>
2015-01-09 04:52:13 +0000

    To cross-compile to JRE 1.6

--- trunk/build.bat
+++ trunk/build.bat
@@ -18,7 +18,7 @@ cd ..\tasks
 del *.class
 cd ..\..\..
 
-javac cvosteen\sqltool\SQLTool.java
+javac -source 1.6 -target 1.6 -bootclasspath "C:\Program Files (x86)\Java\jre6\lib\rt.jar" cvosteen\sqltool\SQLTool.java
 @if errorlevel 1 goto :eof
 
 jar cvfm SQLTool.jar manifest.mf cvosteen\sqltool\*.class cvosteen\sqltool\license.html cvosteen\sqltool\database\*.class cvosteen\sqltool\memory\*.class cvosteen\sqltool\gui\*.class cvosteen\sqltool\gui\components\*.class cvosteen\sqltool\gui\syntax\*.class cvosteen\sqltool\gui\icons\*.png cvosteen\sqltool\task\*.class cvosteen\sqltool\tasks\*.class
